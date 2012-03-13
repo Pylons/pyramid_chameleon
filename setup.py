ee7dd230c3ac66dad63746697be8c157ad6e672f
Reed O'Brien <reed@koansys.com>
2012-03-12 23:04:49 -0700

    suffix .md > .txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
@@ -40,4 +40,3 @@ setup(name='pyramid_chameleon',
       entry_points="""\
       """,
       )
-
