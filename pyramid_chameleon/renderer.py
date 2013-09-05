d1934349aaf657a2589686544f4fe839a99c7838
Bert JW Regeer <bertjw@regeer.org>
2013-09-04 22:50:18 -0600

    Import latest Chameleon changes from Pyramid

--- pyramid_chameleon/renderer.py
+++ pyramid_chameleon/renderer.py
@@ -1,4 +1,5 @@
 import os
+import re
 import threading
 
 import pkg_resources
@@ -20,9 +21,12 @@ from pyramid_chameleon.interfaces import (
 
 @implementer(IChameleonLookup)
 class ChameleonRendererLookup(object):
-    """
-    Chameleon renderer lookup...
-    """
+    spec_re = re.compile(
+        r'(?P<asset>[\w_.:/-]+)'
+        r'(?:\#(?P<defname>[\w_]+))?'
+        r'(\.(?P<ext>.*))'
+        )
+
     def __init__(self, impl, registry):
         self.impl = impl
         self.registry = registry
@@ -55,23 +59,29 @@ class ChameleonRendererLookup(object):
 
         return spec
 
-    @property  # wait until completely necessary to look up translator
+    @property # wait until completely necessary to look up translator
     def translate(self):
         return self.registry.queryUtility(IChameleonTranslate)
 
-    @property  # wait until completely necessary to look up debug_templates
+    @property # wait until completely necessary to look up debug_templates
     def debug(self):
         settings = self.registry.settings
         if settings is None:
             return False
         return settings.get('debug_templates', False)
 
-    @property  # wait until completely necessary to look up reload_templates
+    @property # wait until completely necessary to look up reload_templates
     def auto_reload(self):
         settings = self.registry.settings
         if settings is None:
             return False
         return settings.get('reload_templates', False)
+
+    def _crack_spec(self, spec):
+        asset, macro, ext = self.spec_re.match(spec).group(
+            'asset', 'defname', 'ext'
+            )
+        return asset, macro, ext
 
     def __call__(self, info):
         spec = self.get_spec(info.name, info.package)
@@ -83,58 +93,49 @@ class ChameleonRendererLookup(object):
                 raise ValueError('Missing template file: %s' % spec)
             renderer = registry.queryUtility(ITemplateRenderer, name=spec)
             if renderer is None:
-                renderer = self.impl(spec, self)
+                renderer = self.impl(spec, self, macro=None)
                 # cache the template
-                try:
-                    self.lock.acquire()
+                with self.lock:
                     registry.registerUtility(renderer,
                                              ITemplateRenderer, name=spec)
-                finally:
-                    self.lock.release()
         else:
             # spec is a package:relpath asset spec
             renderer = registry.queryUtility(ITemplateRenderer, name=spec)
             if renderer is None:
+                asset, macro, ext = self._crack_spec(spec)
+                spec_without_macro = '%s.%s' % (asset, ext)
                 try:
-                    package_name, filename = spec.split(':', 1)
-                except ValueError:  # pragma: no cover
+                    package_name, filename = spec_without_macro.split(':', 1)
+                except ValueError: # pragma: no cover
                     # somehow we were passed a relative pathname; this
                     # should die
                     package_name = caller_package(4).__name__
-                    filename = spec
+                    filename = spec_without_macro
                 abspath = pkg_resources.resource_filename(package_name,
                                                           filename)
                 if not pkg_resources.resource_exists(package_name, filename):
                     raise ValueError(
-                        'Missing template asset: %s (%s)' % (spec, abspath))
-                renderer = self.impl(abspath, self)
+                        'Missing template asset: %s (%s)' % (
+                            spec_without_macro, abspath)
+                        )
+                renderer = self.impl(abspath, self, macro=macro)
                 settings = info.settings
                 if not settings.get('reload_assets'):
                     # cache the template
-                    self.lock.acquire()
-                    try:
+                    with self.lock:
                         registry.registerUtility(renderer, ITemplateRenderer,
                                                  name=spec)
-                    finally:
-                        self.lock.release()
 
         return renderer
 
-
 registry_lock = threading.Lock()
 
-
 def template_renderer_factory(info, impl, lock=registry_lock):
-    """
-    ...template renderer factory...
-    """
     registry = info.registry
     lookup = registry.queryUtility(IChameleonLookup, name=info.type)
     if lookup is None:
         lookup = ChameleonRendererLookup(impl, registry)
-        lock.acquire()
-        try:
+        with lock:
             registry.registerUtility(lookup, IChameleonLookup, name=info.type)
-        finally:
-            lock.release()
     return lookup(info)
+
