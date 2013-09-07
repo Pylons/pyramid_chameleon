666ffa746364ef26b42e0ebfd3a633f6d0943db1
Chris McDonough <chrism@plope.com>
2013-09-07 04:07:33 -0400

    change copyright year, remove unused code

--- pyramid_chameleon/views.py
+++
@@ -1,7 +0,0 @@
-from pyramid.view import view_config
-
-## TODO: Add a demo or delete this...
-@view_config(route_name='home', renderer='templates/main.pt')
-def my_view(request):
-    return {'project': 'pyramid_chameleon',
-            'title': "pyramid_chameleon"}
