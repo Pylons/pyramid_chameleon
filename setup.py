4e30547059c91c42b3c9a9eea4336bdbbfc11ad7
Markéta Calábková <meggy.calabkova@gmail.com>
2021-08-03 14:12:29 +0200

    drop python2

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(name='pyramid_chameleon',
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
